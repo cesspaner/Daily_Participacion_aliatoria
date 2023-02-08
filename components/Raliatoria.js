501a4a5c770956109e02b73281b96ab4a0fbaa99
FaustoPalacios <78325627+cesspaner@users.noreply.github.com>
2023-02-07 20:18:30 -0600

    Participacion Aliatoria Daily se separan funciones

--- components/Raliatoria.js
+++ components/Raliatoria.js
@@ -1,4 +1,4 @@
-const nombres = ["Cristopher Cisneros", "Eduardo Luna", "Ivan Solis", "Moises Bermudez","Fausto Palacios"];
+      const nombres = [ "Eduardo Luna", "Ivan Solis","Cristopher Cisneros" ,"Moises Bermudez","Fausto Palacios"];
       const lista = document.getElementById("nombres-lista");
       const generarBoton = document.getElementById("generar-button");
 
@@ -14,37 +14,4 @@ const nombres = ["Cristopher Cisneros", "Eduardo Luna", "Ivan Solis", "Moises Be
         }
       });
 
-      const timer    = document.getElementById("timer");
-      const startBtn = document.getElementById("start");
-      const stopBtn  = document.getElementById("stop");
-      const resetBtn = document.getElementById("reset");
-            
-            let totalSeconds = 900;
-            let intervalId;
-            
-            function startTimer() {
-              intervalId = setInterval(function() {
-                totalSeconds--;
-                const minutes = Math.floor(totalSeconds / 60);
-                const seconds = totalSeconds % 60;
-                timer.innerHTML = `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
-                if (totalSeconds === 0) {
-                  clearInterval(intervalId);
-                }
-              }, 1000);
-            }
-            
-            function stopTimer() {
-              clearInterval(intervalId);
-            }
-            
-            function resetTimer() {
-              clearInterval(intervalId);
-              totalSeconds = 900;
-              timer.innerHTML = "15:00";
-            }
-            
-            startBtn.addEventListener("click", startTimer);
-            stopBtn.addEventListener("click", stopTimer);
-            resetBtn.addEventListener("click", resetTimer);
-                
+      
